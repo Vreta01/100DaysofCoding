420b7b70bfc380bc1010de684d8d95f25703f5e4
Dylan <104588861+Vreta01@users.noreply.github.com>
2023-09-15 16:13:43 -0400

    Add files via upload

---
+++ Remove Duplicates.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = new int[] { 0,0,0,0,0 };
+        int k = RemoveDuplicates(nums);
+        Console.WriteLine(k);
+    }
+    public static int RemoveDuplicates(int[] nums)
+    {
+        int k = nums.Count();
+        Console.WriteLine(k);
+        int p1;
+        int dupCount = 0;
+        for (int i = 0; i < nums.Count() - 1; i++)
+        {
+            dupCount = 0;
+            p1 = nums[i];
+            if (p1 != Int32.MaxValue)
+            {
+                for (int j = i + 1; j < nums.Count(); j++)
+                {
+                    if (nums[j] == p1)
+                    {
+                        dupCount++;
+                    }
+                    if (dupCount > 1)
+                    {
+                        nums[j] = Int32.MaxValue;
+                        k--;
+                        dupCount--;
+                        Console.WriteLine($"nums: {j} removed. P1 = {p1}, k = {k}");
+                    }
+                }
+            }
+
+        }
+        Array.Sort(nums);
+        foreach (int n in nums)
+        {
+            Console.WriteLine($"nums: {n}");
+        }
+        return k;
+    }
+}
