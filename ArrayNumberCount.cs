4585c73737f17acce88f4fdb034b9ffa0b212647
Dylan <104588861+Vreta01@users.noreply.github.com>
2023-09-26 14:24:13 -0400

    Add files via upload

---
+++ ArrayNumberCount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    internal class ArrayNumberCount
+    {
+        public static bool UniqueOccurences(int[] arr)
+        {
+            
+            List<int> counts = new List<int>();
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int currentValue = arr[i];
+                int currentCount = 0;
+                if (!numbers.Contains(currentValue))
+                {
+                    foreach (int number in arr)
+                    {
+                        if (number == currentValue)
+                        { currentCount++; }
+                    }
+
+                    if (counts.Contains(currentCount))
+                    { return false; }
+                    else
+                    {
+                        counts.Add(currentCount);
+                        numbers.Add(currentValue);
+                    }
+                }
+
+            }
+            return true;
+        }
+    }
+}
