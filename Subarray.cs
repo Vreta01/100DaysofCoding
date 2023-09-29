b7037186b3d8aab27985d482366aa30f382456d6
Dylan <104588861+Vreta01@users.noreply.github.com>
2023-09-29 11:43:51 -0400

    Add files via upload

---
+++ Subarray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    internal class Subarray
+    {
+        public static double FindMaxAverage(int[] nums, int k)
+        {
+            double average = 0;
+
+            for (int i = 0; i < k; i++)
+            {
+                average += nums[i];
+            }
+
+            double slidingSum = average;
+            double slidingAverage = 0;
+
+            average = average / k;
+
+            for ( int i = k; i < nums.Length; i++)
+            {
+                slidingSum += nums[i] - nums[i-k];
+                slidingAverage = slidingSum / k;
+                if (slidingAverage > average)
+                {
+                    average = slidingAverage;
+                }
+            }
+            return average;
+        }
+    }
+}
