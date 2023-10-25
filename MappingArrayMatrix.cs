700b26090c344a3315f8edb1e4a36a9b5dd4573a
Dylan <104588861+Vreta01@users.noreply.github.com>
2023-10-25 13:27:02 -0400

    Add files via upload

---
+++ MappingArrayMatrix.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    internal class MappingArrayMatrix
+    {
+        public static int EqualPairs(int[][] grid)
+        {
+            int n = grid.Length;
+            int result = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    int checkCount = 0;
+                    for (int c = 0; c < n; c++)
+                    {
+                        if (grid[i][c] == grid[c][j])
+                        { checkCount++; }
+                    }
+                    if (checkCount == n)
+                    { result++; }
+                }
+            }
+            return result;
+        }
+    }
+}
