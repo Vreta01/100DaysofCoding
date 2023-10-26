c06b5dbe207a6e450b432b376f320c1b755d3a62
Dylan <104588861+Vreta01@users.noreply.github.com>
2023-10-26 11:00:47 -0400

    Add files via upload

---
+++ LinkedListDelete.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    public class ListNode(int val = 0, ListNode? next = null)
+    {
+      public int val = val;
+      public ListNode? next = next;
+    }
+
+    internal class LinkedListDelete
+    {
+        public ListNode DeleteMiddle(ListNode head)
+        {
+
+            int count = 1;
+            if (head.next == null)
+            { return null; }
+            if (head.next.next == null)
+            {
+                head.next = null;
+                return head;
+            }
+
+            ListNode mid = head;
+            ListNode start = head;
+
+            while (head.next != null)
+            {
+                count++;
+                if (count % 2 == 0)
+                { mid = mid.next; }
+                head = head.next;
+            }
+            mid.val = mid.next.val;
+            mid.next = mid.next.next;
+            return start;
+        }
+    }
+}
