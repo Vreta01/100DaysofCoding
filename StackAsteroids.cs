46c90a32fff3228b286a73270dd8355255b6b98a
Dylan <104588861+Vreta01@users.noreply.github.com>
2023-10-31 11:26:51 -0400

    Add files via upload

---
+++ StackAsteroids.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    internal class StackAsteroids
+    {
+        public int[] AsteroidCollision(int[] asteroids)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            foreach (int asteroid in asteroids)
+            {
+                if (asteroid > 0)
+                {
+                    stack.Push(asteroid);
+                }
+                else
+                {
+                    while (stack.Any() && stack.Peek() > 0 && stack.Peek() < Math.Abs(asteroid))
+                    {
+                        stack.Pop();
+                    }
+
+                    if (stack.Count == 0 || stack.Peek() < 0)
+                    { stack.Push(asteroid); }
+                    else if (stack.Peek() == Math.Abs(asteroid))
+                    { stack.Pop(); }
+                }
+            }
+            int[] result = new int[stack.Count];
+            for (int i = stack.Count - 1; i >= 0; i--)
+            { result[i] = stack.Pop(); }
+            return result;
+        }
+    }
+}
