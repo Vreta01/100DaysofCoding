f18a725f4bd8c09b4955d9e85039fc779f1614b2
Dylan <104588861+Vreta01@users.noreply.github.com>
2023-11-02 11:22:41 -0400

    Add files via upload

---
+++ LinkedListMaxPair.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    internal class LinkedListMaxPair
+    {
+        public int PairSum(ListNode head)
+        {
+            Stack<int> nodeStack = new Stack<int>();
+            ListNode slow = head;
+            ListNode fast = head.next;
+            int maxSum = 0;
+
+            nodeStack.Push(slow.val);
+
+            while (fast.next != null)
+            {
+                slow = slow.next;
+                nodeStack.Push(slow.val);
+                fast = fast.next.next;
+            }
+
+            while (slow.next != null)
+            {
+                slow = slow.next;
+                maxSum = Math.Max(maxSum, nodeStack.Pop() + slow.val);
+            }
+            return maxSum;
+        }
+    }
+}
