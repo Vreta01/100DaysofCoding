f265f37c927b49bde9e29d86ffe159ef06c88ff9
Dylan <104588861+Vreta01@users.noreply.github.com>
2023-11-06 12:30:03 -0500

    Add files via upload

---
+++ BinaryTreePathSum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    internal class BinaryTreePathSum
+    {
+        /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+        public class Solution
+        {
+            public int PathSum(TreeNode root, int targetSum)
+            {
+                if (root == null)
+                { return 0; }
+
+                return FindPath(root, targetSum)
+                    + PathSum(root.left, targetSum)
+                    + PathSum(root.right, targetSum);
+            }
+            private static int FindPath(TreeNode root, int sum)
+            {
+                int counter = root.val == sum ? 1 : 0;
+
+                if (root.left != null)
+                { counter += FindPath(root.left, sum - root.val); }
+                if (root.right != null)
+                { counter += FindPath(root.right, sum - root.val); }
+
+                return counter;
+            }
+        }
+    }
+}
