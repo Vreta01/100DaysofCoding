7754e0684b235e36473fd31692e32ea1c94279ec
Dylan <104588861+Vreta01@users.noreply.github.com>
2023-11-07 13:04:08 -0500

    Add files via upload

---
+++ BinaryTreeLongestPath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    internal class BinaryTreeLongestPath
+    {
+        public int LongestZigZag(TreeNode root)
+        {
+
+            int left = ZigOrZag(root, true, 0);
+            int right = ZigOrZag(root, false, 0);
+            int answer = Math.Max(left, right);
+            return answer - 1;
+
+        }
+        public static int ZigOrZag(TreeNode node, bool isLeft, int length)
+        {
+            if (node == null)
+            { return length; }
+            int current = isLeft ? ZigOrZag(node.right, !isLeft, length + 1) : ZigOrZag(node.left, !isLeft, length + 1);
+            int alternate = isLeft ? ZigOrZag(node.left, isLeft, 1) : ZigOrZag(node.right, isLeft, 1);
+            int maxLength = Math.Max(current, alternate);
+            return maxLength;
+        }
+    }
+}
