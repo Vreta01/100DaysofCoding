f86100418e70e9e0d8b79f284a1df148824c4ccd
Dylan <104588861+Vreta01@users.noreply.github.com>
2023-11-08 13:18:36 -0500

    Add files via upload

---
+++ BinarySearchTree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    internal class BinarySearchTree
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null)
+            { return null; }
+
+            root = CheckNode(root, val);
+
+            if (root.val == val)
+            { return root; }
+            else
+            { return null; }
+        }
+        public static TreeNode CheckNode(TreeNode node, int val)
+        {
+
+            if (node.val < val && node.right != null)
+            { node = CheckNode(node.right, val); }
+            if (node.val > val && node.left != null)
+            { node = CheckNode(node.left, val); }
+
+            return node;
+        }
+    }
+}
