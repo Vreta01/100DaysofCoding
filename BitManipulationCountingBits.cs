a77cfb8191a73b081f3e43e5d74ee63c56939f9f
Dylan <104588861+Vreta01@users.noreply.github.com>
2023-11-16 09:28:11 -0500

    Add files via upload

---
+++ BitManipulationCountingBits.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    internal class BitManipulationCountingBits
+    {
+        public int[] CountBits(int n)
+        {
+
+            int[] ans = new int[n + 1];
+
+            for (int i = 0; i <= n; i++)
+            {
+                int sum = 0;
+                int num = i;
+
+                while (num != 0)
+                {
+                    sum += num % 2;
+                    num = num / 2;
+                }
+                ans[i] = sum;
+            }
+            return ans;
+        }
+    }
+}
