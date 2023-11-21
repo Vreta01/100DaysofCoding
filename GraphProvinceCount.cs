8e1fe2c92835b301b329590c3ceac2c2ee07637b
Dylan <104588861+Vreta01@users.noreply.github.com>
2023-11-21 14:03:46 -0500

    Add files via upload

---
+++ GraphProvinceCount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    internal class GraphProvinceCount
+    {
+
+        int[] parent = Array.Empty<int>();
+        public int FindCircleNum(int[][] isConnected)
+        {
+            int n = isConnected.Length;
+            parent = new int[n];
+
+            for (int i = 0; i < n; i++)
+            { parent[i] = i; }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (isConnected[i][j] == 1)
+                    { Union(i, j); }
+                }
+            }
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (FindRoot(i) == i)
+                { count++; }
+            }
+            return count;
+        }
+        private int FindRoot(int x)
+        {
+            if (parent[x] != x)
+            { parent[x] = FindRoot(parent[x]); }
+            return parent[x];
+        }
+        private void Union(int x, int y)
+        {
+            parent[FindRoot(x)] = parent[FindRoot(y)];
+        }
+
+    }
+}
