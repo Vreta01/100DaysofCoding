1900243dfe9f800252e60751493523d99ce07f31
Dylan <104588861+Vreta01@users.noreply.github.com>
2023-11-28 12:22:13 -0500

    Add files via upload

---
+++ BitManipulateFindNum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    internal class BitManipulateFindNum
+    {
+        public int SingleNumber(int[] nums)
+        {
+            List<int> countedNumbers = new List<int>();
+
+            foreach (int number in nums)
+            {
+                if (countedNumbers.Contains(number))
+                { countedNumbers.Remove(number); }
+                else
+                { countedNumbers.Add(number); }
+            }
+            return countedNumbers[0];
+
+        }
+
+        public int SingleNumberBitPath(int[] nums)
+        {
+            var result = 0;
+            foreach (var number in nums)
+            { result ^= number; }
+            return result;
+        }
+    }
+}
