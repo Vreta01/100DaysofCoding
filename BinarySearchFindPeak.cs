12533efd1f3b66ecc029d7da85b6fb2c1b00edb1
Dylan <104588861+Vreta01@users.noreply.github.com>
2023-12-01 11:25:48 -0500

    Add files via upload

---
+++ BinarySearchFindPeak.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    internal class BinarySearchFindPeak
+    {
+        public int FindPeakElement(int[] nums)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+
+            while (left + 1 < right)
+            {
+                var mid = left + (right - left) / 2;
+                if (nums[mid] < nums[mid + 1])
+                { left = mid; }
+                else
+                { right = mid; }
+            }
+            if (nums[left] < nums[right])
+            { return right; }
+            else
+            { return left; }
+        }
+    }
+}
