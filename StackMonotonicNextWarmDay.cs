3cadf7f6cea09d26a165684d59137e3b1406d2f2
Dylan <104588861+Vreta01@users.noreply.github.com>
2023-12-07 11:17:17 -0500

    Add files via upload

---
+++ StackMonotonicNextWarmDay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    internal class StackMonotonicNextWarmDay
+    {
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            int n = temperatures.Length;
+            if (temperatures.Length < 2)
+            { return new int[n]; }
+
+            Stack<int> stackOfIndex = new Stack<int>();
+            int[] holding = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                while (stackOfIndex.Count != 0 && temperatures[stackOfIndex.Peek()] < temperatures[i])
+                {
+                    holding[stackOfIndex.Peek()] = i - stackOfIndex.Peek();
+                    stackOfIndex.Pop();
+                }
+                stackOfIndex.Push(i);
+            }
+            return holding;
+
+        }
+    }
+}
