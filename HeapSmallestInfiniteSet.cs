b84a603d3e352ea1d4c4fd523698d2a07cf661a4
Dylan <104588861+Vreta01@users.noreply.github.com>
2023-12-13 12:56:03 -0500

    Add files via upload

---
+++ HeapSmallestInfiniteSet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Practice
+{
+    internal class HeapSmallestInfiniteSet
+    {
+        private HashSet<int> hash;
+        private PriorityQueue<int, int> heap;
+        private int count;
+
+        public HeapSmallestInfiniteSet()
+        {
+            hash = new HashSet<int>();
+            heap = new PriorityQueue<int, int>();
+            count = 0;
+        }
+
+        public int PopSmallest()
+        {
+            count++;
+            heap.Enqueue(count, count);
+            int removed = heap.Dequeue();
+            hash.Add(removed);
+            return removed;
+
+        }
+
+        public void AddBack(int num)
+        {
+            if (hash.Contains(num))
+            {
+                heap.Enqueue(num, num);
+                hash.Remove(num);
+            }
+
+        }
+    }
+}
